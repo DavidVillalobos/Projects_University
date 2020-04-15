2f37fd3af64951100acb87eee0a18596343a7cf2
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2020-04-15 01:39:24 -0600

    Ignore este commit y el anterior
    me equivoque de repo

--- CodeSource/Vehicle.c
+++
@@ -1,9 +0,0 @@
-#include"Vehicle.h"
-
-void initialize_vehicle(struct Vehicle* car){
-    car = malloc(sizeof(struct Vehicle));
-    car->direction = 0;
-    car->num = 0;
-    car->priority = 0;
-    car->speed = 0;
-}
