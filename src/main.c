67efd014d9c7815ea7c3c2addebf9ae2712696b4
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2020-05-14 03:37:17 -0600

    Preview of Moves
    Una vista previa de los movimientos de los agentes que viajan con curvas o rectos, funciona segun lo esperado

--- src/main.c
+++ src/main.c
@@ -1,13 +1,31 @@
 #include"simulation.h"
 
 int main(int argc , char* argv[]){
+    srand(time(NULL));
     init_configurations();
-    // initializing threads 
-    //start simulation
-    // print matrix  
+    struct agent_t* agent = malloc(sizeof(struct agent_t));
+    agent->type = 1;
+    agent->state = 2;
+    //Puntos iniciales
+    generarPuntos(agent);
+    agent->position[0] = rand() % (map_weight + 1);//Punto aleatorio de inicio
+    agent->position[1] = rand() % (map_height + 1);
+    if(generarRecta(agent)){
+        visualizarRecta(agent);
+    }
+    agent->position[0] =  rand() % (map_weight + 1);;//Punto aleatorio de inicio
+    agent->position[1] =  rand() % (map_height + 1);;
+    agent->state = 1;
+    generarPuntos(agent);
+    if(generarCurva(agent)){
+        visualizarCurva(agent);
+    }
+    // Star_Simulation
     print_map();
+     /*
+    // print matrix  
     if(argc == 2){
         printf("El programa durara: %s segundos", argv[1]);
         sleep(atoi(argv[1]));
-    }
+    }//Initialize */
 }
--- src/simulation.c
+++ src/simulation.c
@@ -2,17 +2,15 @@
 
 // Genera una pared rectangular en los puntos especificados
 void build_wall(int x1, int y1, int x2, int y2){
-    int j;
-    for (int i = y1-1; i < y2; i++){ // Filas    Y
+    int i,j;
+    for (i = y1-1; i < y2; i++){ // Filas    Y
         for (j = x1-1; j < x2; j++){ // Columnas X
-            map[i][j].wall = 1;
+            map[i][j].state = 0;
         }
     }
 }
 
-//Inicializar las configuraciones
 int init_configurations(){
-    // Read configuration files
     // Configuracion del mapa
     char* path = "../test/map.ini";
     FILE *file = fopen(path, "r");
@@ -20,7 +18,7 @@ int init_configurations(){
         printf("Map settings file does not exist in the path: %s\n", path);
         return 0;
     } 
-    fscanf(file, "%d %d", &map_height, &map_weight);
+    fscanf(file, "%d %d", &map_height, &map_weight); 
     map = (struct boxes_t**) malloc(map_height * sizeof(struct boxes_t*));
     int i,j;
     for(i = 0; i < map_height; i++){
@@ -28,8 +26,8 @@ int init_configurations(){
     }
     for (i = 0; i < map_height; i++){
         for (j = 0; j < map_weight; j++){
-            map[i][j].wall = map[i][j].cant = 0;
-            map[i][j].type_agent_infect = -1;
+            map[i][j].cant = 0;
+            map[i][j].state = -1;
             pthread_mutex_init(&map[i][j].id , NULL);
         }
     }
@@ -64,12 +62,11 @@ int init_configurations(){
             groups[i][j].type = type;
             groups[i][j].state = (state == 's')?1:2;
             groups[i][j].velocity = min + rand() % (max + 1 - min);
-            groups[i][j].position[0] = rand() % (map_weight + 1); //X,
+            groups[i][j].position[0] = rand() % (map_weight + 1); //X
             groups[i][j].position[1] = rand() % (map_height + 1); //Y
         }
     }
     fclose(file);
-
     // Configuracion de la enfermedad
     path = "../test/disease.ini";
     file = fopen(path, "r");
@@ -80,7 +77,7 @@ int init_configurations(){
     fscanf(file, "%f", &mortality);
     fscanf(file, "%d", &time_to_die);
     fscanf(file, "%d", &time_to_heal);
-    for (i = 0; i < 4; i++){
+    for(i = 0; i < 4; i++){
         for(j = 0; j < 4; j++){
             fscanf(file, "%f", &contagion_rate[i][j]);
         }
@@ -100,7 +97,18 @@ void print_map(){
     for (i = 0; i < map_height; i++){
         printf("║"); 
         for (j = 0; j < map_weight; j++){
-            printf("%s", (map[i][j].wall)?"║":" ");
+            if(map[i][j].state == 0 || map[i][j].state == -1){
+                printf("%s", (map[i][j].state == 0)?"║":" ");
+            }else{// 1 Sano, 2 Enfermo, 3 Curado
+                if(map[i][j].state == 1){
+                    printf("\x1B[32m");    
+                }else if(map[i][j].state == 2){
+                    printf("\x1B[31m");
+                }else{
+                    printf("\x1B[34m");
+                }
+                printf("*\x1B[0m");
+            }
         }
         printf("║\n");
     }
@@ -109,4 +117,115 @@ void print_map(){
         printf("═");
     }
     printf("╝\n"); 
+}
+
+//Calcula una recta dados dos puntos, el primer punto es 
+//la posicion del agente y el segundo es la nueva posicion
+int generarRecta(struct agent_t* agent){
+    agent->position[2] = 0; //No es cuadratica
+    float x2 = agent->route[0][0]; // Puntos aleatorios en 
+    float y2 = agent->route[1][0]; // el borde de la matriz
+    if(x2 - agent->position[0] != 0){ // Actualiza los valores M y  B siempre que no implique una recta vertical
+        agent->coefficient[1] = (y2 - agent->position[1]) / (x2 - agent->position[0]);
+        agent->coefficient[0] = agent->position[1] - agent->coefficient[1] * agent->position[0];
+        return 1;
+    }
+    return 0;
+}
+
+//Calcula una curva dados tres puntos, el primer punto es la 
+//posicion del agente y los otros son nuevas posiciones de la ruta
+int generarCurva(struct agent_t* agent){
+    float p1x =  agent->route[0][0];
+    float p1y =  agent->route[1][0];
+    float p2x =  agent->route[0][1]; 
+    float p2y =  agent->route[1][1];
+    float p3x =  agent->route[0][2]; 
+    float p3y =  agent->route[1][2];
+    float r1[3], r2[3], det;
+    //+ Generamos sistema de Ecuaciones Lineales
+    //*r 1
+    r1[0] = p1x*p1x - p2x*p2x; 
+    r1[1] = p1x - p2x; 
+    r1[2] = p1y - p2y; 
+    //*r2
+    r2[0] = p2x*p2x - p3x*p3x; 
+    r2[1] = p2x - p3x; 
+    r2[2] = p2y - p3y;
+    //+ Solucion del Sistema de Ecuaciones Lineales
+    det = (r1[0] * r2[1]) - (r2[0] * r1[1]);
+    if(det == 0) return 0;
+    //* a, b, c
+    agent->coefficient[2] = ((r2[1] * r1[2]) - (r1[1] * r2[2])) / det;
+    agent->coefficient[1] = ((r1[0] * r2[2]) - (r2[0] * r1[2])) / det;
+    agent->coefficient[0] = p1y - agent->coefficient[2]*p1x*p1x - agent->coefficient[1]*p1x;
+    return 1;
+}
+
+//Permite ver el camino generado con la funcion
+// hasta n posiciones de un agente
+
+void visualizarRecta(struct agent_t* agent){
+    printf("Forma:  f(x) = %0.1f * x + %0.1f\n", agent->coefficient[1] , agent->coefficient[0]);
+    int x = agent->position[0];
+    float yprev = agent->position[1];
+    float y;
+    while(1){//seguir ejecutando mientras no choque con algo
+        //printf("Funcion:f(%d) = %0.1f * %d + %0.1f\n", x, agent->coefficient[1] , x, agent->coefficient[0]);
+        y = x * agent->coefficient[1] + agent->coefficient[0];
+        //printf("Resultado Real: f(%d) = %0.1f\n", x, y);
+        y = round(y);
+        if(1 < fabs(fabs(yprev) - fabs((y)))){// 1 < | y - newY | ? (El salto es de mas de una posicion)
+            y = yprev - 1;
+            //printf("DOBLE SALTO: Nuevas Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+            printf("Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+            int auxy = y;
+            if( 0 < auxy && auxy < map_height && 0 < x && x < map_weight)
+               map[auxy][x].state = agent->state;
+            else break;
+        }else{
+            printf("Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+            int auxy = y;
+            if( 0 < auxy && auxy < map_height && 0 < x && x < map_weight)
+                map[auxy][x].state = agent->state;
+            else break;
+            x++;
+        }
+        yprev = y;
+    }
+}
+
+void visualizarCurva(struct agent_t* agent){
+    printf("Forma:  f(x) = %0.1f * (x)^2 + %0.1f * x + %0.1f\n", agent->coefficient[2], agent->coefficient[1] , agent->coefficient[0]);
+    int x = agent->position[0];
+    //float yprev = agent->position[1];
+    float y;
+    while(1){//seguir ejecutando mientras no choque con algo
+        //printf("Funcion:f(%d) = %0.1f(%d)^2 + %0.1f * %d + %0.1f\n",x, agent->coefficient[2], x, agent->coefficient[1] , x, agent->coefficient[0]);
+        y = agent->coefficient[2] * x * x + agent->coefficient[1] * x + agent->coefficient[0];
+        //printf("Resultado Real: f(%d) = %0.1f\n", x, y);
+        int auxy = round(y);
+        printf("Posiciones: (x,y) (%d, %d)\n",x, auxy);
+        if( 0 < auxy && auxy < map_height && 0 < x && x < map_weight)
+               map[auxy][x].state = agent->state;
+        else break;
+        //if(1 < fabs(fabs(yprev) - fabs((y)))){// 1 < | y - newY | ? (El salto es de mas de una posicion)
+        //    y = yprev - 1;
+            //printf("DOBLE SALTO: Nuevas Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+            //printf("Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+        //}else{
+            //printf("Posiciones: (x,y) (%d, %0.0f)\n",x, y);
+            x++;
+        //}
+        //yprev = y;
+    }
+}
+
+//Genera 7 Puntos por donde debe pasar un estacional
+void generarPuntos(struct agent_t* agent){
+    for (int i = 0; i < 7; i++){
+        agent->route[0][i] = rand() % (map_weight + 1);
+        agent->route[1][i] = rand() % (map_height + 1);
+    }
+    return;
 }
