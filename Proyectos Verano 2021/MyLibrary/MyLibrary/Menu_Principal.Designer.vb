a6e22e20e92e01298054c4bdf039237be529fc8a
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-01-13 00:51:23 -0600

    Cambio de base de datos Nuevo, Pais, Estado de lectura y Editorial

--- Proyectos Verano 2021/MyLibrary/MyLibrary/Menu_Principal.Designer.vb
+++ Proyectos Verano 2021/MyLibrary/MyLibrary/Menu_Principal.Designer.vb
@@ -28,17 +28,31 @@ Partial Class Menu_Principal
         Me.Button_Nuevo_Autor = New System.Windows.Forms.Button()
         Me.Button_Nuevo_Genero = New System.Windows.Forms.Button()
         Me.Regresar = New System.Windows.Forms.Button()
+        Me.Button_Nuevo_Pais = New System.Windows.Forms.Button()
+        Me.Button_Nueva_Editorial = New System.Windows.Forms.Button()
         Me.MyLibraryDataSet = New MyLibrary.MyLibraryDataSet()
+        Me.MyLibraryDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.LibroBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.LibroTableAdapter = New MyLibrary.MyLibraryDataSetTableAdapters.LibroTableAdapter()
+        Me.AutorBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.AutorTableAdapter = New MyLibrary.MyLibraryDataSetTableAdapters.AutorTableAdapter()
+        Me.MyLibraryDataSetBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MyLibraryDataSet1 = New MyLibrary.MyLibraryDataSet1()
+        Me.LibroBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LibroTableAdapter1 = New MyLibrary.MyLibraryDataSet1TableAdapters.LibroTableAdapter()
         CType(Me.MyLibraryDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MyLibraryDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.LibroBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AutorBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MyLibraryDataSetBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MyLibraryDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LibroBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(311, 21)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(286, 29)
         Me.Label1.TabIndex = 0
@@ -52,7 +66,7 @@ Partial Class Menu_Principal
         Me.Button_Nuevo_Libro.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_Nuevo_Libro.Font = New System.Drawing.Font("Segoe UI", 11.0!)
         Me.Button_Nuevo_Libro.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Button_Nuevo_Libro.Location = New System.Drawing.Point(23, 67)
+        Me.Button_Nuevo_Libro.Location = New System.Drawing.Point(12, 60)
         Me.Button_Nuevo_Libro.Name = "Button_Nuevo_Libro"
         Me.Button_Nuevo_Libro.Size = New System.Drawing.Size(168, 55)
         Me.Button_Nuevo_Libro.TabIndex = 2
@@ -67,7 +81,7 @@ Partial Class Menu_Principal
         Me.Button_Nuevo_Autor.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_Nuevo_Autor.Font = New System.Drawing.Font("Segoe UI", 11.0!)
         Me.Button_Nuevo_Autor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Button_Nuevo_Autor.Location = New System.Drawing.Point(23, 139)
+        Me.Button_Nuevo_Autor.Location = New System.Drawing.Point(12, 140)
         Me.Button_Nuevo_Autor.Name = "Button_Nuevo_Autor"
         Me.Button_Nuevo_Autor.Size = New System.Drawing.Size(168, 53)
         Me.Button_Nuevo_Autor.TabIndex = 3
@@ -82,7 +96,7 @@ Partial Class Menu_Principal
         Me.Button_Nuevo_Genero.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_Nuevo_Genero.Font = New System.Drawing.Font("Segoe UI", 11.0!)
         Me.Button_Nuevo_Genero.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Button_Nuevo_Genero.Location = New System.Drawing.Point(23, 212)
+        Me.Button_Nuevo_Genero.Location = New System.Drawing.Point(12, 213)
         Me.Button_Nuevo_Genero.Name = "Button_Nuevo_Genero"
         Me.Button_Nuevo_Genero.Size = New System.Drawing.Size(168, 52)
         Me.Button_Nuevo_Genero.TabIndex = 4
@@ -97,26 +111,89 @@ Partial Class Menu_Principal
         Me.Regresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Regresar.Font = New System.Drawing.Font("Segoe UI", 11.0!)
         Me.Regresar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Regresar.Location = New System.Drawing.Point(23, 437)
+        Me.Regresar.Location = New System.Drawing.Point(12, 436)
         Me.Regresar.Name = "Regresar"
         Me.Regresar.Size = New System.Drawing.Size(168, 52)
         Me.Regresar.TabIndex = 5
         Me.Regresar.Text = "Regresar"
         Me.Regresar.UseVisualStyleBackColor = False
         '
+        'Button_Nuevo_Pais
+        '
+        Me.Button_Nuevo_Pais.BackColor = System.Drawing.Color.Moccasin
+        Me.Button_Nuevo_Pais.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button_Nuevo_Pais.FlatAppearance.BorderSize = 0
+        Me.Button_Nuevo_Pais.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_Nuevo_Pais.Font = New System.Drawing.Font("Segoe UI", 11.0!)
+        Me.Button_Nuevo_Pais.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button_Nuevo_Pais.Location = New System.Drawing.Point(12, 288)
+        Me.Button_Nuevo_Pais.Name = "Button_Nuevo_Pais"
+        Me.Button_Nuevo_Pais.Size = New System.Drawing.Size(168, 52)
+        Me.Button_Nuevo_Pais.TabIndex = 6
+        Me.Button_Nuevo_Pais.Text = "Nuevo Pais"
+        Me.Button_Nuevo_Pais.UseVisualStyleBackColor = False
+        '
+        'Button_Nueva_Editorial
+        '
+        Me.Button_Nueva_Editorial.BackColor = System.Drawing.Color.Moccasin
+        Me.Button_Nueva_Editorial.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button_Nueva_Editorial.FlatAppearance.BorderSize = 0
+        Me.Button_Nueva_Editorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_Nueva_Editorial.Font = New System.Drawing.Font("Segoe UI", 11.0!)
+        Me.Button_Nueva_Editorial.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button_Nueva_Editorial.Location = New System.Drawing.Point(12, 365)
+        Me.Button_Nueva_Editorial.Name = "Button_Nueva_Editorial"
+        Me.Button_Nueva_Editorial.Size = New System.Drawing.Size(168, 52)
+        Me.Button_Nueva_Editorial.TabIndex = 7
+        Me.Button_Nueva_Editorial.Text = "Nueva Editorial"
+        Me.Button_Nueva_Editorial.UseVisualStyleBackColor = False
+        '
         'MyLibraryDataSet
         '
         Me.MyLibraryDataSet.DataSetName = "MyLibraryDataSet"
         Me.MyLibraryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
+        'MyLibraryDataSetBindingSource
+        '
+        Me.MyLibraryDataSetBindingSource.DataSource = Me.MyLibraryDataSet
+        Me.MyLibraryDataSetBindingSource.Position = 0
+        '
         'LibroBindingSource
         '
         Me.LibroBindingSource.DataMember = "Libro"
-        Me.LibroBindingSource.DataSource = Me.MyLibraryDataSet
+        Me.LibroBindingSource.DataSource = Me.MyLibraryDataSetBindingSource
         '
         'LibroTableAdapter
         '
         Me.LibroTableAdapter.ClearBeforeFill = True
+        '
+        'AutorBindingSource
+        '
+        Me.AutorBindingSource.DataMember = "Autor"
+        Me.AutorBindingSource.DataSource = Me.MyLibraryDataSetBindingSource
+        '
+        'AutorTableAdapter
+        '
+        Me.AutorTableAdapter.ClearBeforeFill = True
+        '
+        'MyLibraryDataSetBindingSource1
+        '
+        Me.MyLibraryDataSetBindingSource1.DataSource = Me.MyLibraryDataSet
+        Me.MyLibraryDataSetBindingSource1.Position = 0
+        '
+        'MyLibraryDataSet1
+        '
+        Me.MyLibraryDataSet1.DataSetName = "MyLibraryDataSet1"
+        Me.MyLibraryDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LibroBindingSource1
+        '
+        Me.LibroBindingSource1.DataMember = "Libro"
+        Me.LibroBindingSource1.DataSource = Me.MyLibraryDataSet1
+        '
+        'LibroTableAdapter1
+        '
+        Me.LibroTableAdapter1.ClearBeforeFill = True
         '
         'Menu_Principal
         '
@@ -125,6 +202,8 @@ Partial Class Menu_Principal
         Me.BackColor = System.Drawing.Color.CornflowerBlue
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.ClientSize = New System.Drawing.Size(802, 511)
+        Me.Controls.Add(Me.Button_Nueva_Editorial)
+        Me.Controls.Add(Me.Button_Nuevo_Pais)
         Me.Controls.Add(Me.Regresar)
         Me.Controls.Add(Me.Button_Nuevo_Genero)
         Me.Controls.Add(Me.Button_Nuevo_Autor)
@@ -136,7 +215,12 @@ Partial Class Menu_Principal
         Me.Name = "Menu_Principal"
         Me.Text = "MyLibrary"
         CType(Me.MyLibraryDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MyLibraryDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.LibroBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AutorBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MyLibraryDataSetBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MyLibraryDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LibroBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -147,7 +231,16 @@ Partial Class Menu_Principal
     Friend WithEvents Button_Nuevo_Autor As Button
     Friend WithEvents Button_Nuevo_Genero As Button
     Friend WithEvents Regresar As Button
+    Friend WithEvents Button_Nuevo_Pais As Button
+    Friend WithEvents Button_Nueva_Editorial As Button
+    Friend WithEvents MyLibraryDataSetBindingSource As BindingSource
     Friend WithEvents MyLibraryDataSet As MyLibraryDataSet
     Friend WithEvents LibroBindingSource As BindingSource
     Friend WithEvents LibroTableAdapter As MyLibraryDataSetTableAdapters.LibroTableAdapter
+    Friend WithEvents AutorBindingSource As BindingSource
+    Friend WithEvents AutorTableAdapter As MyLibraryDataSetTableAdapters.AutorTableAdapter
+    Friend WithEvents MyLibraryDataSetBindingSource1 As BindingSource
+    Friend WithEvents MyLibraryDataSet1 As MyLibraryDataSet1
+    Friend WithEvents LibroBindingSource1 As BindingSource
+    Friend WithEvents LibroTableAdapter1 As MyLibraryDataSet1TableAdapters.LibroTableAdapter
 End Class
--- Proyectos Verano 2021/MyLibrary/MyLibrary/Menu_Principal.vb
+++ Proyectos Verano 2021/MyLibrary/MyLibrary/Menu_Principal.vb
@@ -9,17 +9,37 @@
         Add_Book.Show()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button_Nuevo_Autor.Click
+    Private Sub Button_Nuevo_Autor_Click(sender As Object, e As EventArgs) Handles Button_Nuevo_Autor.Click
         Me.Hide()
         Add_Autor.Show()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button_Nuevo_Genero.Click
+    Private Sub Button_Nuevo_Genero_Click(sender As Object, e As EventArgs) Handles Button_Nuevo_Genero.Click
         Me.Hide()
         Add_Gender.Show()
     End Sub
 
+    Private Sub Button_Nuevo_Pais_Click(sender As Object, e As EventArgs) Handles Button_Nuevo_Pais.Click
+        Me.Hide()
+        Add_Countries.Show()
+    End Sub
+
+    Private Sub Button_Nueva_Editorial_Click(sender As Object, e As EventArgs) Handles Button_Nueva_Editorial.Click
+        Me.Hide()
+        Add_Editorial.Show()
+    End Sub
+
     Private Sub Menu_Principal_FormClosing(sender As Object, e As EventArgs) Handles MyBase.Closed
         Title.Close()
+    End Sub
+
+    Private Sub Menu_Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'MyLibraryDataSet1.Libro' Puede moverla o quitarla según sea necesario.
+        Me.LibroTableAdapter1.Fill(Me.MyLibraryDataSet1.Libro)
+        'TODO: esta línea de código carga datos en la tabla 'MyLibraryDataSet.Autor' Puede moverla o quitarla según sea necesario.
+        Me.AutorTableAdapter.Fill(Me.MyLibraryDataSet.Autor)
+        'TODO: esta línea de código carga datos en la tabla 'MyLibraryDataSet.Libro' Puede moverla o quitarla según sea necesario.
+        Me.LibroTableAdapter.Fill(Me.MyLibraryDataSet.Libro)
+
     End Sub
 End Class
